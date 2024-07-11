0e42737bc87a938fb0f1e54bfe3d002025b71ed1
jensen-dong <jensendong141@gmail.com>
2024-07-10 21:24:36 -0700

    feat: add ship count

--- src/components/StarshipList.jsx
+++ src/components/StarshipList.jsx
@@ -6,16 +6,19 @@ const StarshipList = ({ starships }) => {
   }
 
   return (
-    <div className="card-container">
-      {starships.map((starship, index) => (
-        <StarshipCard
-          key={index}
-          name={starship.name}
-          starship_class={starship.starship_class}
-          manufacturer={starship.manufacturer}
-          model={starship.model}
-        />
-      ))}
+    <div>
+      <h4 className="ship-count">There are {starships.length} matching starships</h4>
+      <div className="card-container">
+        {starships.map((starship, index) => (
+          <StarshipCard
+            key={index}
+            name={starship.name}
+            starship_class={starship.starship_class}
+            manufacturer={starship.manufacturer}
+            model={starship.model}
+          />
+        ))}
+      </div>
     </div>
   );
 };
